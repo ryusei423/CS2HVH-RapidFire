5f5ea9c8068b447f6f3d8bcd2fbb270b9a9470a9
流星423 <72623417+ryusei423@users.noreply.github.com>
2025-02-12 09:34:29 +0800

    ？

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
+using Microsoft.Extensions.Logging;
+
+namespace Rapid;
+public class HelloWorldPlugin : BasePlugin
+{
+    public override string ModuleName => "Rapid";
+	public override string ModuleAuthor => "mETE0R";
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool hotReload)
+    {
+        RegisterListener<Listeners.OnTick>(() =>
+        {
+            var dogshit = Utilities.GetPlayers();
+
+
+
+            for (var i = 0; i < dogshit.Count; i++)
+            {
+                if (dogshit[i] == null ||
+                dogshit[i].IsBot ||
+                !dogshit[i].PawnIsAlive) continue;
+
+                var weapon = dogshit[i].Pawn.Get().WeaponServices.ActiveWeapon.Get();
+
+                if (weapon.Clip1 <= 0) continue;
+
+                weapon.NextPrimaryAttackTick = 0;
+                weapon.NextPrimaryAttackTickRatio = 0;
+                weapon.NextSecondaryAttackTick = 0;
+                weapon.NextSecondaryAttackTickRatio = 0;
+
+                Utilities.SetStateChanged(weapon, "CBasePlayerWeapon", "m_nNextPrimaryAttackTick");
+                Utilities.SetStateChanged(weapon, "CBasePlayerWeapon", "m_flNextPrimaryAttackTickRatio");
+                Utilities.SetStateChanged(weapon, "CBasePlayerWeapon", "m_nNextSecondaryAttackTick");
+                Utilities.SetStateChanged(weapon, "CBasePlayerWeapon", "m_flNextSecondaryAttackTickRatio");
+            }
+
+
+        });
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# CS2HVH-RapidFire
-重新使hvh变得无聊
+
+最近比较流行的服务端速射的一种实现方式
+肯定不是最好的实现，但是它确实工作，我只是无聊
+
+**如果你只想快速参考原理:**
+
+    weapon.NextPrimaryAttackTick = 0;
+    Utilities.SetStateChanged(weapon, "CBasePlayerWeapon", "m_nNextPrimaryAttackTick");
+
